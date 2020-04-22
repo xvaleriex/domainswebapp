4b10f30e2d7228efd42d6848d38f775f4b21fa2f
xvaleriex <valerykyyv@gmail.com>
2020-04-22 13:38:43 +0200

    feat: add tests

--- domainslist/views.py
+++ domainslist/views.py
@@ -2,20 +2,18 @@ from django.shortcuts import render
 from domainslist.models import Domain, DomainFlag
 from datetime import datetime
 
+
 def domainslist_index(request):
-	not_deleted_domains = Domain.objects.filter(datetime_deletion__isnull=True)
-	all_flags = DomainFlag.objects.all()
-	active_flags = all_flags.exclude(
-		datetime_from__lte=datetime.now(), datetime_to__gte=datetime.now())	
-	context = {
-		'domains': not_deleted_domains,
-		'flags': all_flags 
-	}
-	return render(request, 'domainslist_index.html', context)
+    not_deleted_domains = Domain.objects.filter(datetime_deletion__isnull=True)
+    context = {
+        'domains': not_deleted_domains
+    }
+    return render(request, 'domainslist_index.html', context)
+
 
 def domain_detail(request, id):
-	domain = Domain.objects.get(pk=id)
-	context = {
-		'domain': domain
-	}
-	return render(request, 'domain_detail.html', context)
+    domain = Domain.objects.get(pk=id)
+    context = {
+        'domain': domain
+    }
+    return render(request, 'domain_detail.html', context)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/xvaleriex/PycharmProjects/domains_web/venv"
+set -gx VIRTUAL_ENV "/Users/xvaleriex/PycharmProjects/proj_niccz/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
